3f30d8d01f9dd4010239d472e77399a4476fafdf
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-06-25 21:53:52 +0300

    кени

--- plugins/main-dylux.js
+++ plugins/main-dylux.js
@@ -4,15 +4,7 @@ let handler = async (m, { conn }) => {
 m.reply(`
 ≡  *DyLuxᴮᴼᵀ ┃ SUPPORT*
 
-◈ ━━━━━━━━━━━━━━━━━━━━ ◈
-▢ Grupo *1*
-https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU
 
-▢ Grupo *2*
-https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE
-
-▢ Grupo *NSFW* 🔞
-https://chat.whatsapp.com/F0JTTyZ3hsoL7OlU8TEpuH
 
 ◈ ━━━━━━━━━━━━━━━━━━━━ ◈
 ▢ Todos los Grupos
