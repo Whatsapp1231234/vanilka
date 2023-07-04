4fac88deb5776a129f99bd216ace50a6e2c6b29d
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-04 11:20:24 +0300

    лопм

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `\n🟢 vanilka ${muptime}\n\n ┃ 👑Лёлька👑`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
