c9e347256c9def4bdc6635208139d2ef47d47f47
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-10-31 16:36:06 +0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -12,7 +12,9 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            const linkThisGroup2 = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
+            if (m.text.includes(linkThisGroup2)) return !0
         }
         await conn.reply(m.chat, `*≡ Enlace Detectado*
             
